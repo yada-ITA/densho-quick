0610bcab43f1f95da129f0b81d34195f450a4013
yada-ITA <yadaita+github@gmail.com>
2016-03-01 08:53:38 +0900

    [wip]中断close対応

--- app/models/request_application.rb
+++ app/models/request_application.rb
@@ -3,7 +3,7 @@ class RequestApplication < ActiveRecord::Base
   belongs_to :project, class_name: "Dept"
   mount_uploader :filename, FileUploader
 
-  validates :management_no, uniqueness: true
+  validates :management_no, uniqueness: true, presence: true
 
   def self.closed(id)
     request_application = RequestApplication.find(id)
@@ -11,7 +11,37 @@ class RequestApplication < ActiveRecord::Base
     request_application.save
   end
 
-  def initial?
-    self.flows.order(:order).last.history_no == 1
+
+  # 通常closeかどうか
+  def normal_closed?
+    self.close? && self.flows.order(:history_no).last.order == FlowOrder.maximum(:order)
   end
+
+  # 中断closeかどうか
+  def interrupt_closeed?
+    self.close? && self.flows.order(:history_no).last.order != FlowOrder.maximum(:order)
+  end
+
+
+  def close_status
+    ("済" if normal_closed? )|| ("中断" if interrupt_closeed? )
+  end
+
+  def interrupt_permit?
+  #書記状態ではなくが、フローの一番最初の部署に処理がある状態のとき
+   ! initial? &&  self.flows.order(:history_no).last.order == 1
+  end
+
+  def delete_permit?
+    initial?
+  end
+
+
+  private
+
+    def initial?
+      self.flows.order(:order).last.history_no == 1
+    end
+
+
 end
