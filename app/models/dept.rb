05da060ada3c2a29b0265fe384da1a0c5c30424e
yada-ITA <yadaita+github@gmail.com>
2016-03-14 15:16:10 +0900

    プロジェクト名・部署登録時の、名称重複エラー、ブランクエラーを表示(S-01038対応)

--- app/models/dept.rb
+++ app/models/dept.rb
@@ -1,4 +1,7 @@
 class Dept < ActiveRecord::Base
   scope :projects, -> { where(project: true) }
   scope :departments, -> { where(project: false) }
+
+  validates :name, uniqueness: true, presence: true
+
 end
