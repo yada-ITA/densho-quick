6875cbbec3ef4b597a198b91e824ac2ea2408413
yada-ITA <yadaita+github@gmail.com>
2016-03-17 15:37:45 +0900

    要求書作成時に、機種コードを登録できるようにする（S-01033対応）

--- app/controllers/request_applications_controller.rb
+++ app/controllers/request_applications_controller.rb
@@ -112,6 +112,6 @@ class RequestApplicationsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def request_application_params
-    params.require(:request_application).permit(:management_no, :emargency, :filename, :request_date, :preferred_date, :close, :project_id, :memo, :vendor_code)
+    params.require(:request_application).permit(:management_no, :emargency, :filename, :request_date, :preferred_date, :close, :project_id, :memo, :vendor_code, :model_id)
   end
 end
