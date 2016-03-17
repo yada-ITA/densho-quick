9f7957e4c597f49f1e3412d22131c574025ec89c
yada-ITA <yadaita+github@gmail.com>
2016-03-17 16:31:30 +0900

    要求書の更新時に、ベンダーコードが更新できるように修正（S-01032対応）

--- app/controllers/request_applications_controller.rb
+++ app/controllers/request_applications_controller.rb
@@ -1,5 +1,6 @@
 class RequestApplicationsController < ApplicationController
   before_action :set_request_application, only: [:show, :edit, :update, :destroy, :regist, :reject, :interrupt, :first_to_revert]
+  before_action :set_vendor_id, only: [:update]
 
   # GET /request_applications
   # GET /request_applications.json
@@ -46,7 +47,7 @@ class RequestApplicationsController < ApplicationController
   # PATCH/PUT /request_applications/1.json
   def update
     respond_to do |format|
-      if @request_application.update(request_application_params)
+      if @request_application.update(request_application_params) && set_vendor_id
         format.html { redirect_to @request_application, notice: 'Request application was successfully updated.' }
         format.json { render :show, status: :ok, location: @request_application }
       else
@@ -108,10 +109,16 @@ class RequestApplicationsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_request_application
     @request_application = RequestApplication.find(params[:id])
+    @request_application.vendor_code = Vendor.find(@request_application.vendor_id).try(:code)
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def request_application_params
     params.require(:request_application).permit(:management_no, :emargency, :filename, :request_date, :preferred_date, :close, :project_id, :memo, :vendor_code, :model_id)
+  end
+
+  def set_vendor_id
+    @request_application.vendor_id = Vendor.find_by(code: params["request_application"]["vendor_code"]).try(:id)
+    @request_application.save
   end
 end
